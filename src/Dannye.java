59ff878865d9163cfb0d3c72f483e0487dade4db
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-11 12:59:59 +0300

    inter-com

--- src/Dannye.java
+++ src/Dannye.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Dannye {
+public class Dannye  {
+    private int gotVypuska;
+    private String model;
+    private int sena;
+    private String svet;
+
+    @Override
+    public String toString() {
+        return "Dannye{" +
+                "gotVypuska = " + gotVypuska +
+                ", mode l= '" + model + '\'' +
+                ", sena = " + sena +
+                ", svet = ' " + svet + '\'' +
+                '}';
+    }
+
+    public Dannye(int gotVypuska, String model, int sena, String svet) {
+       // super(id, number);
+        this.gotVypuska = gotVypuska;
+        this.model = model;
+        this.sena = sena;
+        this.svet = svet;
+    }
+
+
+    public int getGotVypuska() {
+        return gotVypuska;
+    }
+
+    public void setGotVypuska(int gotVypuska) {
+        this.gotVypuska = gotVypuska;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSena() {
+        return sena;
+    }
+
+    public void setSena(int sena) {
+        this.sena = sena;
+    }
+
+    public String getSvet() {
+        return svet;
+    }
+
+    public void setSvet(String svet) {
+        this.svet = svet;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.security.KeyStore;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<Car,Dannye> mashyna = new HashMap<>();
+        mashyna.put(new Car(1,222),new Dannye(2022,"Ford mustang GT500",30000,"Kara"));
+        mashyna.put(new Car(2,333),new Dannye(2022,"BMW X6",40000,"Kara"));
+        mashyna.put(new Car(3,444),new Dannye(2022,"AUDI A7",50000,"Kara"));
+        mashyna.put(new Car(4,555),new Dannye(2022,"Mercedes S500",30000,"Kara"));
+        for (Map.Entry<Car, Dannye> entry: mashyna.entrySet()) {
+            System.out.println(entry);
+        }
     }
 }
